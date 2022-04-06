45d597978581b445ea4a600e920da13834cd6f3b
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-04-06 10:03:27 -0700

    boilerPlate for the project

---
+++ script.js
@@ -0,0 +1,3 @@
+const puzzleBoard = document.querySelector('#puzzle-board');
+const solveButton = document.querySelector('#solve-button');
+const squares = 81;  //as there are 9 by 9 squares in the board
