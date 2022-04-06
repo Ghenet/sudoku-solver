0c19f0c15e1811d02e12c08144dc5af50ae4b959
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2022-04-06 10:10:18 -0700

    creates the 81 input squares in the puzzleBoard

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const puzzleBoard = document.querySelector('#puzzle-board');
 const solveButton = document.querySelector('#solve-button');
 const squares = 81;  //as there are 9 by 9 squares in the board
+
+//loop over to create the 81 squares in the puzzleBoard
+for(let i=0; i<squares; i++) {
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'number');
+    inputElement.setAttribute('min', 0);
+    inputElement.setAttribute('max', 9);
+    puzzleBoard.appendChild(inputElement);
+}
